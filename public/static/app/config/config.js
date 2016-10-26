dbed45a0b77405173df6b5045ce55701984bd02c
brucecham <brucecham@qq.com>
2016-10-26 10:11:31 +0800

    add file

---
+++ public/static/app/config/config.js
@@ -0,0 +1,41 @@
+var appUnlogin = angular.module('qmgj_unlogin', ['ui.router']);
+appUnlogin.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/login');
+    $stateProvider
+        .state('login', {
+            url: '/login',
+            templateUrl: '/static/app/view/login.html',
+            controller: 'loginController'
+        })
+        .state('reg', {
+            url: '/reg',
+            templateUrl: '/static/app/view/reg.html',
+            controller: 'regController'
+        })
+        .state('home', {
+            url: '/home',
+            templateUrl: '/static/app/view/home.html',
+            controller: 'homeController'
+        })
+        .state('list',{
+            url: '/list',
+            templateUrl: '/static/app/view/list.html',
+            controller: 'listController'
+        })
+
+}]);
+var appLogin = angular.module('qmgj_login', ['ui.router']);
+appLogin.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: '/static/app/view/home.html',
+            controller: 'homeController'
+        })
+        .state('list',{
+            url: '/list',
+            templateUrl: '/static/app/view/list.html',
+            controller: 'listController'
+        })
+}]);
---
+++ public/static/app/controller/listController.js
@@ -0,0 +1,5 @@
+function listControllerFn($scope,$state){
+	
+}
+appLogin.controller('listController', ['$scope', '$state', listControllerFn]);
+appUnlogin.controller('listController', ['$scope', '$state', listControllerFn]);
